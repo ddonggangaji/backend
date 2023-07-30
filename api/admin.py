56759f9afc0b677f9feed07fca7417d4fa1f9782
JHeaon <j3heawon@gmail.com>
2023-07-30 12:23:14 +0900

    Feat: create db model and regist model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from models import User, Service, Category
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    name = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=100)
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=100)
+    context = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
